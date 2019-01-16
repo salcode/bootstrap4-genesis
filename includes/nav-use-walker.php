0cb89e176936fad9067974b5e3f821dafe901743
Sal Ferrarello <sal@ironcodestudio.com>
2019-01-16 16:48:03 -0500

    Add code to use custom Bootstrap 4 Nav Walker
    See #20

---
+++ includes/nav-use-walker.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Nav use Custom Walker.
+ *
+ * @package bootstrap4genesis
+ */
+
+// filter menu args for bootstrap walker and other settings
+add_filter( 'wp_nav_menu_args', 'bs4g_wp_nav_menu_args' );
+
+function bs4g_wp_nav_menu_args( $args ) {
+	if (
+		'primary' === $args['theme_location']
+		|| 'secondary' === $args['theme_location']
+	) {
+		$args['depth']  = 2;
+		$args['before'] = '';
+		$args['menu_class'] = 'navbar-nav ' . $args['menu_class'];
+		$args['walker'] = new BS4G_Nav_Walker();
+	}
+	return $args;
+}
