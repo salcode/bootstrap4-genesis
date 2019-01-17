92320cb5f3f0be71a1f1ba287732fe5f8660c489
Sal Ferrarello <sal@ironcodestudio.com>
2019-01-17 10:38:33 -0500

    Add additional CSS classes to markup
    See #8

--- includes/css-classes.php
+++ includes/css-classes.php
@@ -6,6 +6,18 @@
  */
 add_filter( 'genesis_attr_site-header', 'bs4g_genesis_attr_css_modifications', 10, 3 );
 add_filter( 'genesis_attr_title-area', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_site-inner', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_content-sidebar-wrap', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_content', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_sidebar-primary', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_entry', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_entry-header', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_entry-image', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_entry-title', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_entry-content', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+add_filter( 'genesis_attr_cpt-archive-description', 'bs4g_genesis_attr_css_modifications', 10, 3 );
+
+
 
 /**
  * Modify Genesis Class Attributes.
@@ -18,7 +30,14 @@ add_filter( 'genesis_attr_title-area', 'bs4g_genesis_attr_css_modifications', 10
 function bs4g_genesis_attr_css_modifications( $attr, $context ) {
 	$css_mapping = apply_filters( 'bootstrap4_genesis_css_mapping', array(
 		// Context    => classname to add,
-		'site-header' => 'navbar fixed-top navbar-dark bg-dark',
+		'content' => '',
+		'content-sidebar-wrap' => 'row justify-content-center',
+		'entry' => 'pb-5 mb-5',
+		'entry-title' => '',
+		'entry-image' => 'img-fluid mb-3',
+		'sidebar-primary' => '',
+		'site-header' => 'navbar navbar-dark bg-dark navbar-expand-lg',
+		'site-inner'  => 'container pt-3 pt-md-5',
 		'title-area'  => 'navbar-brand',
 	) );
 	$attr['class'] .= ' ' . $css_mapping[ $context ] ?? '';
