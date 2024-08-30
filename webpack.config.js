4368d4bea18671386f896058c74cdce1006211c6
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-08-30 15:19:25 -0400

    Use MiniCssExtractPlugin to create style.css
    Use MiniCssExtractPlugin to create style.css from example.css
    
    See #75

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'production',
@@ -6,6 +7,7 @@ module.exports = {
   entry: [
     './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
     './js/custom/main.js',
+    './sass/example.css',
   ],
   externals: {
     jquery: 'jQuery',
@@ -13,5 +15,18 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'js'),
     filename: 'javascript.min.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../style.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
 };
