b385b228f279722dee8ed6ce2336254e01d9c27c
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-08-30 13:52:42 -0400

    Mark jquery as an external dependency
    Mark jquery as an external dependency so webpack does NOT include it in
    the built javascript
    
    We load jquery separately, it should not be part of our theme.
    
    See #75

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+  externals: {
+    jquery: 'jQuery',
+  },
   output: {
     path: path.resolve(__dirname, 'js'),
     filename: 'javascript.min.js',
